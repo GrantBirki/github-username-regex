e7cd4b0ca604301184a2b38c610f678d1c96f919
GrantBirki <grantbirki@github.com>
2023-12-01 22:48:21 -0700

    fmt

--- module.js
+++ module.js
@@ -1,1 +1,1 @@
-export default /^(?:[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}|[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*(_[a-zA-Z0-9]+))$/i
+export default /^(?:[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}|[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*(_[a-zA-Z0-9]+))$/i;
